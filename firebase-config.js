fb4451a186d6398b4b6083aebf4b3ad7ae7c6e51
armanieric03 <armanieric03@gmail.com>
2025-04-15 15:48:20 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDjD6VMIRxgDq13yXjMYeYI2b7-QqqdVI",
+  authDomain: "canal-denuncias-poli26.firebaseapp.com",
+  databaseURL: "https://canal-denuncias-poli26-default-rtdb.firebaseio.com",
+  projectId: "canal-denuncias-poli26",
+  storageBucket: "canal-denuncias-poli26.firebasestorage.app",
+  messagingSenderId: "102848286523",
+  appId: "1:102848286523:web:4b539fe5ec3e15d7e3311e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
